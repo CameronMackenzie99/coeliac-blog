b991eba14dcecac5639caf8a456bfd66737da3d4
CameronMackenzie99 <29843733+CameronMackenzie99@users.noreply.github.com>
2024-07-05 21:15:47 +0100

    Add footer and icons

---
+++ next-app/app/_components/Footer/index.tsx
@@ -0,0 +1,31 @@
+import React, { PropsWithoutRef } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import EmailLogo from '../Logos/Email'
+import InstaLogo from '../Logos/Insta'
+
+export const Footer = () => {
+  return (
+    <footer className="border-t sticky top-[100vh]">
+      <section className="flex w-full items-center">
+        <p className="basis-1/3 text-xs text-slate-400">
+          Website by <br />{' '}
+          <a className="text-slate-500" href="cameronmackenzie.dev">
+            Cameron Mackenzie
+          </a>
+        </p>
+        <p className="flex basis-1/3 text-xs justify-center">© Coeliac Ducky 2024</p>
+        <div className="flex gap-2 items-center justify-end basis-1/3">
+          <a href="https://www.instagram.com/coeliacducky/" target="_blank">
+            <InstaLogo className="h-[16px] w-[16px] fill-slate-700 hover:fill-slate-400" />
+          </a>
+          <a href="mailto:coeliacducky@gmail.com" target="_blank">
+            <EmailLogo className="h-[18px] w-[18px] fill-slate-700 hover:fill-slate-400" />
+          </a>
+        </div>
+      </section>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ next-app/app/_components/Logos/Email.tsx
@@ -0,0 +1,13 @@
+function EmailLogo({ className }: React.ComponentPropsWithoutRef<'svg'>): React.ReactNode {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      fill="currentColor"
+      viewBox="0 0 24 24"
+    >
+      <path d="M0 3v18h24V3H0zm21.518 2L12 12.713 2.482 5h19.036zM2 19V7.183l10 8.104 10-8.104V19H2z" />
+    </svg>
+  )
+}
+export default EmailLogo
