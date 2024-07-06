dbab866414d7583d77a72b9465c3f3d2b550b6ef
CameronMackenzie99 <29843733+CameronMackenzie99@users.noreply.github.com>
2024-07-06 14:55:39 +0100

    Add search params

---
+++ next-app/app/_components/Search/search.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { ChangeEvent, useCallback } from 'react'
+import { Place } from '../../../payload-types'
+
+export function Search({ places }: { places: Place[] }) {
+  const router = useRouter()
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+
+  // Get a new searchParams string by merging the current
+  // searchParams with a provided key/value pair
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString())
+      params.set(name, value)
+
+      return params.toString()
+    },
+    [searchParams],
+  )
+
+  const locations = Array.from(new Set(places.map(place => place.location)))
+
+  const searchLocation = (value: string | undefined): void => {
+    if (!value) return
+    const query = createQueryString('location', value)
+    router.push(pathname + '?' + query)
+  }
+
+  return (
+    <section className="flex flex-col w-fit">
+      <label htmlFor="locations" />
+      Find somewhere in...
+      <select
+        name="locations"
+        id="locations"
+        defaultValue={undefined}
+        onChange={$event => searchLocation($event.target.value)}
+      >
+        <option value={undefined}></option>
+        {locations.map((location, i) => {
+          return (
+            <option key={i} value={location!}>
+              {location}
+            </option>
+          )
+        })}
+      </select>
+    </section>
+  )
+}
--- next-app/app/places/page.tsx
+++ next-app/app/places/page.tsx
@@ -1,12 +1,12 @@
 import { notFound } from 'next/navigation'
 import { fetchPlaces } from '../_api/fetchPages'
 import Link from 'next/link'
-import { Button } from '../_components/Button'
 import { Tag } from '../_components/Tag/Tag'
 import { formatDate } from '../_utils/utils'
+import Image from 'next/image'
+import { Search } from '../_components/Search/search'
 
 export default async function Page() {
-  // const page = await fetchPlace(slug, isDraftMode)
   const places = await fetchPlaces()
 
   if (places === null || places.length === 0) {
@@ -16,19 +16,36 @@ export default async function Page() {
   return (
     <>
       <h1>Places to eat</h1>
+      <Search places={places} />
       {places.map((place, i) => (
         <div
           key={i}
           className="border-2 hover:bg-slate-100 hover:cursor-pointer shadow-[5px_5px_0_0_rgba(0,0,0,0.2)] hover:shadow-[8px_8px_0_0_rgba(0,0,0,0.2)]"
         >
           <Link href={`places/${place.slug}`}>
-            <div className="p-4">
-              <h2>{place.name}</h2>
-              <p className="text-xs italic">{formatDate(place.createdAt)}</p>
-              <div className="flex gap-2">
-                {place.tags?.split(',').map((tag, i) => {
-                  return <Tag key={i} tag={tag} />
-                })}
+            <div className="flex p-4 justify-between">
+              <div className="max-w-64">
+                <h2>{place.name}</h2>
+                <div className="flex flex-col not-prose gap-2 pb-2">
+                  <p className="text-xs">Location: {place.location}</p>
+                  <p className="text-xs italic">
+                    Last visited: {formatDate(place.lastDateOfVisit)}
+                  </p>
+                </div>
+                <div className="flex gap-2 flex-wrap">
+                  {place.tags?.split(',').map((tag, i) => {
+                    return <Tag key={i} tag={tag} />
+                  })}
+                </div>
+              </div>
+              <div>
+                <Image
+                  src="/bluejoanna.png"
+                  className="border"
+                  alt="blue joanna"
+                  width={100}
+                  height={100}
+                />
               </div>
             </div>
           </Link>
