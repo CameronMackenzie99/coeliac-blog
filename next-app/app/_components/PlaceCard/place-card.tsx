d13f69bffc655f667f91c27115638682dc1f5772
CameronMackenzie99 <29843733+CameronMackenzie99@users.noreply.github.com>
2024-07-07 18:46:18 +0100

    Fix undefined

--- next-app/app/_components/PlaceCard/place-card.tsx
+++ next-app/app/_components/PlaceCard/place-card.tsx
@@ -30,8 +30,8 @@ export const PlaceCard = async ({ place }: PlaceCardProps): Promise<React.JSX.El
         <div>
           <div className="relative h-60 w-60 hidden md:flex">
             <Image
-              src={`http://localhost:3000${thumbnail.url}`}
-              alt={thumbnail.alt!}
+              src={`http://localhost:3000${thumbnail?.url ?? ''}`}
+              alt={thumbnail?.alt ?? ''}
               fill
               className="object-contain object-right self-center"
             />
