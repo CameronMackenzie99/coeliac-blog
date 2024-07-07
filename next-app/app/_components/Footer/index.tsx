bdaea0e7f8dbe5ecab2c84c7b9929ef7f7160750
CameronMackenzie99 <29843733+CameronMackenzie99@users.noreply.github.com>
2024-07-07 18:49:54 +0100

    footer

--- next-app/app/_components/Footer/index.tsx
+++ next-app/app/_components/Footer/index.tsx
@@ -8,7 +8,7 @@ export const Footer = () => {
   return (
     <footer className="border-t border-slate-600 mt-4 sticky top-[100vh]">
       <section className="flex w-full items-center">
-        <p className="basis-1/3 text-xs text-slate-500">
+        <p className="basis-1/3 text-md text-slate-500">
           Website by <br />{' '}
           <a
             className="text-slate-500 hover:underline"
@@ -18,13 +18,13 @@ export const Footer = () => {
             Cameron Mackenzie
           </a>
         </p>
-        <p className="flex basis-1/3 text-xs justify-center">© Coeliac Ducky 2024</p>
+        <p className="flex basis-1/3 text-md text-center justify-center">© Coeliac Ducky 2024</p>
         <div className="flex gap-2 items-center justify-end basis-1/3">
           <a href="https://www.instagram.com/coeliacducky/" target="_blank" aria-label="instagram">
-            <InstaLogo className="h-[16px] w-[16px] fill-slate-700 hover:fill-slate-400" />
+            <InstaLogo className="h-[32px] w-[32px] fill-slate-700 hover:fill-slate-400" />
           </a>
           <a href="mailto:coeliacducky@gmail.com" target="_blank" aria-label="email">
-            <EmailLogo className="h-[18px] w-[18px] fill-slate-700 hover:fill-slate-400" />
+            <EmailLogo className="h-[38px] w-[38px] fill-slate-700 hover:fill-slate-400" />
           </a>
         </div>
       </section>
