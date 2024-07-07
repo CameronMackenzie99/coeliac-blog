51af9c4b0685ff6adcf7500bde6199294ad4e44d
CameronMackenzie99 <29843733+CameronMackenzie99@users.noreply.github.com>
2024-07-07 18:50:26 +0100

    font size

--- next-app/app/_components/PlaceCard/place-card.tsx
+++ next-app/app/_components/PlaceCard/place-card.tsx
@@ -17,7 +17,7 @@ export const PlaceCard = async ({ place }: PlaceCardProps): Promise<React.JSX.El
       <div className="flex justify-between p-4">
         <div className="max-w-1/2">
           <div className="flex flex-col not-prose gap-2 pb-2">
-            <h2 className="text-4xl font-bold">{place.name}</h2>
+            <h2 className="text-3xl font-bold">{place.name}</h2>
             <p className="text-md">Location: {place.location}</p>
             <p className="text-md italic">Last visited: {formatDate(place.lastDateOfVisit)}</p>
           </div>
