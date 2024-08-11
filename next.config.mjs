fa3846f89dcd2469b2c1b16322ce61b806822836
CameronMackenzie99 <29843733+CameronMackenzie99@users.noreply.github.com>
2024-08-11 17:04:00 +0100

    Remove headers

--- next.config.mjs
+++ next.config.mjs
@@ -7,25 +7,6 @@ import dotenvExpand from 'dotenv-expand'
 dotenvExpand.expand({ parsed: { ...process.env } })
 
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: 'www.coeliacducky.com' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-          { key: 'Accept', value: 'image/*' },
-        ],
-      },
-    ]
-  },
   images: {
     remotePatterns: [
       {
